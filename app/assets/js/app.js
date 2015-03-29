29b0578cb5b0598fd0b9af24d509fc25b6e3b754
Jonathan Shih <joncshih@gmail.com>
2015-03-28 22:12:58 -0700

    parallax jumbotron scroll

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
+    parallaxScroll();
     contactFormCallback();
 });
+
+function parallaxScroll() {
+    $window = $(window);
+    $bg_img = $(".jumbotron-background");
+    var isMobile = /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+
+    $window.on("scroll", function() {
+        var top = $window.scrollTop();
+        if (!isMobile) {
+            $bg_img.css('transform', 'translate3d(0, '+top/3+'px, 0)')
+                   .css('opacity', Math.min(1-top/2500, 1));
+        }
+    });
+}
 
 function contactFormCallback() {
     $("#contact").submit(function(event) {
